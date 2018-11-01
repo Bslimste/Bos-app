d2c95ff5fa680d7f153f58df84cb6d26ed042e71
Bert de Boer <bertde.boer@hotmail.com>
2018-11-01 19:28:21 +0100

    merging

--- app/screens/Exchange.js
+++ app/screens/Exchange.js
@@ -1,35 +1,190 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, FlatList, TouchableHighlight, Dimensions, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableHighlight,
+  Dimensions,
+  Image
+} from "react-native";
 import { Toolbar } from "react-native-material-ui";
 import LocalStorage from "../config/localStorage.js";
 import Api from "../config/api.js";
 import { NavigationActions } from "react-navigation";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { TabView, TabBar, SceneMap } from "react-native-tab-view";
 
 export default class Exchange extends Component {
   constructor() {
     super();
-    this.state = {"dataArray": []}
+    this.state = {
+      index: 0,
+      routes: [
+        { key: "asked", title: "Asked" },
+        { key: "offered", title: "Offered" }
+      ]
+    };
     ls = LocalStorage.getInstance();
     api = Api.getInstance();
     api.callApi("getAllNecessityRequests", "POST", {}, response => {
-        console.log(response['response'])
-        this.setState({
-            dataArray: response['response']
-        });
-    });
-  }
-
-  componentWillMount() {
-    api = Api.getInstance();
-    api.callApi("getAllNecessityRequests", "POST", {}, response => {
-        this.setState({
-            dataArray: response['response']
-        });
+      completeArray = response["response"];
+      offeredArray = [];
+      askedArray = [];
+      for (let index in completeArray) {
+        entry = completeArray[index];
+        if (entry.offered) {
+          offeredArray.push(entry);
+        } else {
+          askedArray.push(entry);
+        }
+      }
+      this.setState({
+        dataArrayOffered: offeredArray,
+        dataArrayAsked: askedArray
+      });
     });
   }
 
   render() {
+    const OfferedRoute = () => (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.dataArrayOffered}
+          keyExtractor={item => "" + item.id}
+          numColumns={1}
+          renderItem={({ item }) => (
+            <View style={styles.cardContainer}>
+              <Image
+                source={{ uri: item.profilePhoto }}
+                resizeMode="cover"
+                style={{ width: 50, height: 50, borderRadius: 30, margin: 3 }}
+              />
+              <View
+                style={{
+                  alignItems: "flex-start",
+                  width: Dimensions.get("window").width - 110
+                }}
+              >
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                    fontSize: 18,
+                    color: "black"
+                  }}
+                >
+                  {item.title}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 13,
+                    color: "black"
+                  }}
+                >
+                  {item.location}
+                </Text>
+              </View>
+              <TouchableHighlight
+                onPress={() =>
+                  this.props.navigation.navigate("RequestDetail", {
+                    id: item.id,
+                    owner: item.owner,
+                    ownerId: item.ownerId,
+                    location: item.location,
+                    profilePhoto: item.profilePhoto,
+                    title: item.title,
+                    description: item.description,
+                    necessity: item.necessity,
+                    offered: item.offered,
+                    picture: item.picture
+                  })
+                }
+                style={{
+                  flex: 1,
+                  alignItems: "flex-end",
+                  width: 30
+                }}
+              >
+                <Icon
+                  size={25}
+                  name={"arrow-right"}
+                  style={{ color: "grey" }}
+                />
+              </TouchableHighlight>
+            </View>
+          )}
+        />
+      </View>
+    );
+    const AskedRoute = () => (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.dataArrayAsked}
+          keyExtractor={item => "" + item.id}
+          numColumns={1}
+          renderItem={({ item }) => (
+            <View style={styles.cardContainer}>
+              <Image
+                source={{ uri: item.profilePhoto }}
+                resizeMode="cover"
+                style={{ width: 50, height: 50, borderRadius: 30, margin: 3 }}
+              />
+              <View
+                style={{
+                  alignItems: "flex-start",
+                  width: Dimensions.get("window").width - 110
+                }}
+              >
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                    fontSize: 18,
+                    color: "black"
+                  }}
+                >
+                  {item.title}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 13,
+                    color: "black"
+                  }}
+                >
+                  {item.location}
+                </Text>
+              </View>
+              <TouchableHighlight
+                onPress={() =>
+                  this.props.navigation.navigate("RequestDetail", {
+                    id: item.id,
+                    owner: item.owner,
+                    ownerId: item.ownerId,
+                    location: item.location,
+                    profilePhoto: item.profilePhoto,
+                    title: item.title,
+                    description: item.description,
+                    necessity: item.necessity,
+                    offered: item.offered,
+                    picture: item.picture
+                  })
+                }
+                style={{
+                  flex: 1,
+                  alignItems: "flex-end",
+                  width: 30
+                }}
+              >
+                <Icon
+                  size={25}
+                  name={"arrow-right"}
+                  style={{ color: "grey" }}
+                />
+              </TouchableHighlight>
+            </View>
+          )}
+        />
+      </View>
+    );
     return (
       <View style={{ width: "100%", height: "100%" }}>
         <Toolbar
@@ -37,64 +192,14 @@ export default class Exchange extends Component {
           centerElement="Exchange"
           onLeftElementPress={() => this.props.navigation.toggleDrawer()}
         />
-        <View style={styles.container}>
-          <FlatList
-            data={this.state.dataArray}
-            keyExtractor={item => "" + item.id}
-            numColumns={1}
-            renderItem={ ({item}) => (
-                        <View style={styles.cardContainer}>
-                            <Image
-                                source={{ uri: item.profilePhoto }}
-                                resizeMode="cover"
-                                style={{ width: 50, height: 50, borderRadius:30, margin: 3 }}
-                            />
-                            <View style={{
-                                    alignItems: 'flex-start',
-                                    width: Dimensions.get("window").width - 110
-                                }}>
-                                <Text style={{ 
-                                    fontWeight: "bold",
-                                    fontSize: 18,
-                                    color: "black",
-                                    }}>
-                                    {item.title}
-                                </Text>
-                                <Text style={{ 
-                                    fontSize: 13,
-                                    color: "black",
-                                    }}>
-                                    {item.location}
-                                </Text>
-                            </View>
-                            <TouchableHighlight
-                                onPress={() =>
-                                    this.props.navigation.navigate("RequestDetail",
-                                            {
-                                                id: item.id,
-                                                owner: item.owner,
-                                                ownerId: item.ownerId,
-                                                location: item.location,
-                                                profilePhoto: item.profilePhoto,
-                                                title: item.title,
-                                                description: item.description,
-                                                necessity: item.necessity
-                                            }
-                                        )
-                                }
-                                style={{
-                                    flex:1,
-                                    alignItems: "flex-end",
-                                    width: 30
-                                }}
-                            >
-                            <Icon size={25} name={"arrow-right"} style={{ color: "grey" }} />
-                            </TouchableHighlight>
-                        </View> 
-                )
-            }
-          />
-        </View>
+        <TabView
+          navigationState={this.state}
+          renderScene={SceneMap({
+            asked: AskedRoute,
+            offered: OfferedRoute
+          })}
+          onIndexChange={index => this.setState({ index })}
+        />
       </View>
     );
   }
@@ -118,6 +223,6 @@ const styles = StyleSheet.create({
     width: Dimensions.get("window").width,
     height: 70,
     flex: 1,
-    flexDirection: 'row'
+    flexDirection: "row"
   }
 });
